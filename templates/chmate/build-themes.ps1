023c2f95f9fc7b018152b78fc0ad61a10aeeadf7
hanubeki <hanubeki@gmail.com>
2026-04-30 18:33:04 +0900

    Modify some ps1 scripts

--- templates/chmate/build-themes.ps1
+++ templates/chmate/build-themes.ps1
@@ -1,6 +1,6 @@
 Remove-Item -Path ".\theme" -Recurse
 
-~\Downloads\base16-builder-go_0.3.0_windows_amd64.exe -schemes-dir ..\..\schemes
+D:\Soft\base16-builder-go\base16-builder-go_0.3.0_windows_amd64.exe -schemes-dir ..\..\schemes
 
 Get-ChildItem -File -Path ".\theme\base16" | New-Item -Path ".\theme" -Name { $_.Name -replace ".day.txt","" -replace ".night.txt","" } -ItemType "Directory"
 Get-ChildItem -File -Path ".\theme\base16" | Move-Item -Path { ".\theme\base16\" + $_.Name } -Destination { ".\theme\" + ( $_.Name -replace ".day.txt","" -replace ".night.txt","" ) }
--- templates/tinted-shell-pwsh/build-themes.ps1
+++ templates/tinted-shell-pwsh/build-themes.ps1
@@ -1,3 +1,3 @@
 Remove-Item -Path ".\scripts" -Recurse
 
-~\Downloads\base16-builder-go_0.3.0_windows_amd64.exe -schemes-dir ..\..\schemes
+D:\Soft\base16-builder-go\base16-builder-go_0.3.0_windows_amd64.exe -schemes-dir ..\..\schemes
