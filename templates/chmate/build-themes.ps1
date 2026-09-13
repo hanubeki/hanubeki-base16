d91cbd37651e3a7c3a403303a4ec76c4fdfb7573
hanubeki <hanubeki@gmail.com>
2026-09-13 20:15:31 +0900

    Add (semi-)working tinted8 chmate themes

--- templates/chmate/build-themes.ps1
+++ templates/chmate/build-themes.ps1
@@ -1,10 +1,10 @@
 Remove-Item -Path ".\theme" -Recurse
 
-# tinted-builder-rust doesn't support {{scheme-is-light-variant}}, {{scheme-is-dark-variant}} and {{option.is-dark-variant}} in filename
-# C:\Soft\tinted-builder-rust\tinted-builder-rust.exe build --schemes-dir ..\..\schemes .
+C:\Soft\tinted-builder-rust\tinted-builder-rust.exe build --schemes-dir ..\..\schemes .
 
-C:\Soft\base16-builder-go\base16-builder-go_0.3.0_windows_amd64.exe -schemes-dir ..\..\schemes
-
-Get-ChildItem -File -Path ".\theme\base16" | New-Item -Path ".\theme" -Name { $_.Name -replace ".day.txt","" -replace ".night.txt","" } -ItemType "Directory"
-Get-ChildItem -File -Path ".\theme\base16" | Move-Item -Path { ".\theme\base16\" + $_.Name } -Destination { ".\theme\" + ( $_.Name -replace ".day.txt","" -replace ".night.txt","" ) }
-Remove-Item -Path ".\theme\base16"
+Get-ChildItem -Directory -Path ".\theme" | ForEach-Object {
+    Push-Location $_
+    & ".\renamer.ps1"
+    Remove-Item -Path ".\renamer.ps1"
+    Pop-Location
+}
